7c90f2b50893492deafcccc6982bce66a76667ee
Bruno Bragerolli Marques <bruno.marques@ciee.ong.br>
2023-11-22 18:14:55 -0300

    ajusta chave e ajusta json

--- index.js
+++ index.js
@@ -12,52 +12,27 @@ const openai = new OpenAI(OPENAI_API_KEY)
 
 app.use(cors())
 
-// POST /pergunte-ao-chatgpt
-app.post('/pergunte-ao-chatgpt', async (req, res) => {
-
-    const { prompt } = req.body
-
-    // Escolha dos parâmetros
-    const model = 'gpt-3.5-turbo'
-    const role = 'user'
-    const max_tokens = 50
-
-    // Comunicação com o ChatGPT
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: role, content: prompt}],
-        model: model,
-        max_tokens: max_tokens,
-    })
-
-    res.json({completion: completion.choices[0].message.content})
-    })
-
 app.post('/gerar-historia/:tema', async (req, res) => {
-
-    const { tema } = req.params;
-
-    // Configuração do prompt para a geração de texto
-    const prompt = `Escreva uma história infantil sobre ${tema} com 150 palavras. Era uma vez...`
-    const model = 'gpt-3.5-turbo'
-    const role = 'user'
-    const max_tokens = 200;
-
     try {
-        // Solicitação para a API GPT-3
+        const { tema } = req.params;
+        const prompt = `Escreva uma história infantil sobre ${tema} com 150 palavras. Era uma vez...`;
+        const model = 'gpt-3.5-turbo';
+        const role = 'user';
+        const max_tokens = 500; // Aumente o número de tokens permitidos
+
         const completion = await openai.chat.completions.create({
-            messages: [{ role: role, content: prompt}],
+            messages: [{ role: role, content: prompt }],
             model: model,
             max_tokens: max_tokens,
-        })
+        });
 
-        // Retorna a parte gerada da história
-        res.json({ completion: completion.choices[0].message.content })
+        const generatedString = completion.choices[0].message.content;
+
+        res.json({ completion: generatedString });
     } catch (error) {
-        console.error(error)
-        res.status(500).send('Erro ao gerar história.')
+        console.error(error);
+        res.status(500).send('Erro ao gerar história.');
     }
-})
-
-// Colocamos o servidor em execução na porta 4000
-const PORT = 4000
-app.listen(PORT, () => console.log(`Em execução na porta ${PORT}`))
+});
+    const PORT = 4000
+    app.listen(PORT, () => console.log(`Em execução na porta ${PORT}`))
